ffadfe6f527dead2f7484619e36d2f019c9f1388
adjprtma <djitothebeat123@gmail.com>
2021-10-14 22:12:41 +0700

    Telah menyelesaikan video ke 6 dan 7 tentang laravel pada pertemuan 6

--- coba-laravel/app/Models/Post.php
+++ coba-laravel/app/Models/Post.php
@@ -2,31 +2,13 @@
 
 namespace App\Models;
 
-class Post 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
 {
-    private static $blog_posts = [
-        [
-            "title" => "Judul Post Pertama",
-            "slug" => "Judul Post Pertama",
-            "author" => "Adji Pratama",
-            "body" => "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam quos voluptatum officiis alias veniam maxime aut voluptates odio dolorum id molestias recusandae nesciunt laudantium cupiditate at, est corrupti commodi, in repudiandae. Aspernatur nesciunt dignissimos voluptatum officia necessitatibus ab cumque eos optio earum alias, officiis magnam dolore nulla modi distinctio. Ipsa nesciunt assumenda officiis alias magni rerum maiores porro obcaecati, laboriosam earum odit, officia repudiandae, vitae quas dignissimos. Vel, quos consectetur neque rem repellat, fugiat maxime voluptatibus nobis eos dolorum est"
-        ],
-        [
-            "title" => "Judul Post Kedua",
-            "slug" => "Judul Post Kedua",
-            "author" => "Andika Kurniawan",
-            "body" => "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam quos voluptatum officiis alias veniam maxime aut voluptates odio dolorum id molestias recusandae nesciunt laudantium cupiditate at, est corrupti commodi, in repudiandae. Aspernatur nesciunt dignissimos voluptatum officia necessitatibus ab cumque eos optio earum alias, officiis magnam dolore nulla modi distinctio. Ipsa nesciunt assumenda officiis alias magni rerum maiores porro obcaecati, laboriosam earum odit, officia repudiandae, vitae quas dignissimos. Vel, quos consectetur neque rem repellat, fugiat maxime voluptatibus nobis eos dolorum est"
-        ],
-    ];
+    use HasFactory;
 
-    public static function all() 
-    {
-        return collect(self::$blog_posts);
-    }
-
-    public static function find($slug)
-    {
-        $posts = static::all();
-        return $posts->firstWhere('slug', $slug);
-    }
+    // protected $fillable = ['title', 'excerpt', 'body'];
+    protected $guarded = ['id'];
 }
