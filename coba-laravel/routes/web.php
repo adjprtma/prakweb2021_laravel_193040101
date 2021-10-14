0ba1aeb04015282f5664aed8f1024e22cd07f4d9
adjprtma <djitothebeat123@gmail.com>
2021-10-14 14:04:56 +0700

    Sudah menyelesaikan video ke 5 tentang laravel pada pertemuan 6

--- coba-laravel/routes/web.php
+++ coba-laravel/routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\PostController;
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -30,54 +33,5 @@ Route::get('/about', function () {
 
 
 
-Route::get('/blog', function () {
-    $blog_posts = [
-        [
-            "title" => "Judul Post Pertama",
-            "slug" => "Judul Post Pertama",
-            "author" => "Adji Pratama",
-            "body" => "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam quos voluptatum officiis alias veniam maxime aut voluptates odio dolorum id molestias recusandae nesciunt laudantium cupiditate at, est corrupti commodi, in repudiandae. Aspernatur nesciunt dignissimos voluptatum officia necessitatibus ab cumque eos optio earum alias, officiis magnam dolore nulla modi distinctio. Ipsa nesciunt assumenda officiis alias magni rerum maiores porro obcaecati, laboriosam earum odit, officia repudiandae, vitae quas dignissimos. Vel, quos consectetur neque rem repellat, fugiat maxime voluptatibus nobis eos dolorum est"
-        ],
-        [
-            "title" => "Judul Post Kedua",
-            "slug" => "Judul Post Kedua",
-            "author" => "Andika Kurniawan",
-            "body" => "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam quos voluptatum officiis alias veniam maxime aut voluptates odio dolorum id molestias recusandae nesciunt laudantium cupiditate at, est corrupti commodi, in repudiandae. Aspernatur nesciunt dignissimos voluptatum officia necessitatibus ab cumque eos optio earum alias, officiis magnam dolore nulla modi distinctio. Ipsa nesciunt assumenda officiis alias magni rerum maiores porro obcaecati, laboriosam earum odit, officia repudiandae, vitae quas dignissimos. Vel, quos consectetur neque rem repellat, fugiat maxime voluptatibus nobis eos dolorum est"
-        ],
-    ];
-
-    return view('posts', [
-        "title" => "Posts",
-        "posts" => $blog_posts
-    ]);
-});
-
-// Halaman single post
-Route::get('posts/{slug}', function($slug) {
-    $blog_posts = [
-        [
-            "title" => "Judul Post Pertama",
-            "slug" => "Judul Post Pertama",
-            "author" => "Adji Pratama",
-            "body" => "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam quos voluptatum officiis alias veniam maxime aut voluptates odio dolorum id molestias recusandae nesciunt laudantium cupiditate at, est corrupti commodi, in repudiandae. Aspernatur nesciunt dignissimos voluptatum officia necessitatibus ab cumque eos optio earum alias, officiis magnam dolore nulla modi distinctio. Ipsa nesciunt assumenda officiis alias magni rerum maiores porro obcaecati, laboriosam earum odit, officia repudiandae, vitae quas dignissimos. Vel, quos consectetur neque rem repellat, fugiat maxime voluptatibus nobis eos dolorum est"
-        ],
-        [
-            "title" => "Judul Post Kedua",
-            "slug" => "Judul Post Kedua",
-            "author" => "Andika Kurniawan",
-            "body" => "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam quos voluptatum officiis alias veniam maxime aut voluptates odio dolorum id molestias recusandae nesciunt laudantium cupiditate at, est corrupti commodi, in repudiandae. Aspernatur nesciunt dignissimos voluptatum officia necessitatibus ab cumque eos optio earum alias, officiis magnam dolore nulla modi distinctio. Ipsa nesciunt assumenda officiis alias magni rerum maiores porro obcaecati, laboriosam earum odit, officia repudiandae, vitae quas dignissimos. Vel, quos consectetur neque rem repellat, fugiat maxime voluptatibus nobis eos dolorum est"
-        ],
-    ];
-
-    $new_post = [];
-    foreach($blog_posts as $post) {
-        if($post["slug"] === $slug) {
-            $new_post = $post;
-        }
-    }
-
-    return view('post', [
-        "title" => "Single Post",
-        "post" => $new_post
-    ]);
-});
+Route::get('/blog', [PostController::class, 'index']);
+Route::get('posts/{slug}', [PostController::class, 'show']);
