927a5590b3047dc8df62f669726151fb4f8c21da
adjprtma <djitothebeat123@gmail.com>
2021-10-20 13:56:33 +0700

    Sudah menyelesaikan video ke 9 dari pertemuan 7

--- coba-laravel/database/seeders/DatabaseSeeder.php
+++ coba-laravel/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Category;
+use App\Models\Post;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +17,63 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        User::create([
+            'name' => 'Adji Pratama',
+            'email' => 'adji28@gmail.com',
+            'password' => bcrypt('12345')
+        ]);
+
+        User::create([
+            'name' => 'Rizal Muhaimin',
+            'email' => 'imin28@gmail.com',
+            'password' => bcrypt('12345')
+        ]);
+
+        Category::create([
+            'name' => 'Web Programming',
+            'slug' => 'web-programming'
+        ]);
+
+        Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal'
+        ]);
+
+        Post::create([
+            'title' => 'Judul Pertama',
+            'slug' => 'judul-pertama',
+            'excerpt' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam quos voluptatum officiis alias veniam maxime aut voluptates odio dolorum id molestias recusandae nesciunt laudantium cupiditate at, est corrupti commodi, in repudiandae. Aspernatur nesciunt dignissimos voluptatum officia necessitatibus ab cumque eos optio earum alias, officiis magnam dolore nulla modi distinctio. Ipsa nesciunt assumenda officiis alias magni rerum maiores porro obcaecati, laboriosam earum odit, officia repudiandae, vitae quas dignissimo',
+            'body' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam quos voluptatum officiis alias veniam maxime aut voluptates odio dolorum id molestias recusandae nesciunt laudantium cupiditate at, est corrupti commodi, in repudiandae. Aspernatur nesciunt dignissimos voluptatum officia necessitatibus ab cumque eos optio earum alias, officiis magnam dolore nulla modi distinctio. Ipsa nesciunt assumenda officiis alias magni rerum maiores porro obcaecati, laboriosam earum odit, officia repudiandae, vitae quas dignissimo Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam quos voluptatum officiis alias veniam maxime aut voluptates odio dolorum id molestias recusandae nesciunt laudantium cupiditate at, est corrupti commodi, in repudiandae. Aspernatur nesciunt dignissimos voluptatum officia necessitatibus ab cumque eos optio earum alias, officiis magnam dolore nulla modi distinctio. Ipsa nesciunt assumenda officiis alias magni rerum maiores porro obcaecati, laboriosam earum odit, officia repudiandae, vitae quas dignissimo',
+            'category_id' => 1,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Ke Dua',
+            'slug' => 'judul-ke-dua',
+            'excerpt' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam quos voluptatum officiis alias veniam maxime aut voluptates odio dolorum id molestias recusandae nesciunt laudantium cupiditate at, est corrupti commodi, in repudiandae. Aspernatur nesciunt dignissimos voluptatum officia necessitatibus ab cumque eos optio earum alias, officiis magnam dolore nulla modi distinctio. Ipsa nesciunt assumenda officiis alias magni rerum maiores porro obcaecati, laboriosam earum odit, officia repudiandae, vitae quas dignissimo',
+            'body' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam quos voluptatum officiis alias veniam maxime aut voluptates odio dolorum id molestias recusandae nesciunt laudantium cupiditate at, est corrupti commodi, in repudiandae. Aspernatur nesciunt dignissimos voluptatum officia necessitatibus ab cumque eos optio earum alias, officiis magnam dolore nulla modi distinctio. Ipsa nesciunt assumenda officiis alias magni rerum maiores porro obcaecati, laboriosam earum odit, officia repudiandae, vitae quas dignissimo Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam quos voluptatum officiis alias veniam maxime aut voluptates odio dolorum id molestias recusandae nesciunt laudantium cupiditate at, est corrupti commodi, in repudiandae. Aspernatur nesciunt dignissimos voluptatum officia necessitatibus ab cumque eos optio earum alias, officiis magnam dolore nulla modi distinctio. Ipsa nesciunt assumenda officiis alias magni rerum maiores porro obcaecati, laboriosam earum odit, officia repudiandae, vitae quas dignissimo',
+            'category_id' => 1,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Ke Tiga',
+            'slug' => 'judul-ke-tiga',
+            'excerpt' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam quos voluptatum officiis alias veniam maxime aut voluptates odio dolorum id molestias recusandae nesciunt laudantium cupiditate at, est corrupti commodi, in repudiandae. Aspernatur nesciunt dignissimos voluptatum officia necessitatibus ab cumque eos optio earum alias, officiis magnam dolore nulla modi distinctio. Ipsa nesciunt assumenda officiis alias magni rerum maiores porro obcaecati, laboriosam earum odit, officia repudiandae, vitae quas dignissimo',
+            'body' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam quos voluptatum officiis alias veniam maxime aut voluptates odio dolorum id molestias recusandae nesciunt laudantium cupiditate at, est corrupti commodi, in repudiandae. Aspernatur nesciunt dignissimos voluptatum officia necessitatibus ab cumque eos optio earum alias, officiis magnam dolore nulla modi distinctio. Ipsa nesciunt assumenda officiis alias magni rerum maiores porro obcaecati, laboriosam earum odit, officia repudiandae, vitae quas dignissimo Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam quos voluptatum officiis alias veniam maxime aut voluptates odio dolorum id molestias recusandae nesciunt laudantium cupiditate at, est corrupti commodi, in repudiandae. Aspernatur nesciunt dignissimos voluptatum officia necessitatibus ab cumque eos optio earum alias, officiis magnam dolore nulla modi distinctio. Ipsa nesciunt assumenda officiis alias magni rerum maiores porro obcaecati, laboriosam earum odit, officia repudiandae, vitae quas dignissimo',
+            'category_id' => 2,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Ke Empat',
+            'slug' => 'judul-ke-empat',
+            'excerpt' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam quos voluptatum officiis alias veniam maxime aut voluptates odio dolorum id molestias recusandae nesciunt laudantium cupiditate at, est corrupti commodi, in repudiandae. Aspernatur nesciunt dignissimos voluptatum officia necessitatibus ab cumque eos optio earum alias, officiis magnam dolore nulla modi distinctio. Ipsa nesciunt assumenda officiis alias magni rerum maiores porro obcaecati, laboriosam earum odit, officia repudiandae, vitae quas dignissimo',
+            'body' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam quos voluptatum officiis alias veniam maxime aut voluptates odio dolorum id molestias recusandae nesciunt laudantium cupiditate at, est corrupti commodi, in repudiandae. Aspernatur nesciunt dignissimos voluptatum officia necessitatibus ab cumque eos optio earum alias, officiis magnam dolore nulla modi distinctio. Ipsa nesciunt assumenda officiis alias magni rerum maiores porro obcaecati, laboriosam earum odit, officia repudiandae, vitae quas dignissimo Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam quos voluptatum officiis alias veniam maxime aut voluptates odio dolorum id molestias recusandae nesciunt laudantium cupiditate at, est corrupti commodi, in repudiandae. Aspernatur nesciunt dignissimos voluptatum officia necessitatibus ab cumque eos optio earum alias, officiis magnam dolore nulla modi distinctio. Ipsa nesciunt assumenda officiis alias magni rerum maiores porro obcaecati, laboriosam earum odit, officia repudiandae, vitae quas dignissimo',
+            'category_id' => 2,
+            'user_id' => 2
+        ]);
     }
 }
