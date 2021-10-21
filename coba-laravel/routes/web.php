82e2887b045589d11cb799ca0cbb76f357c6b53c
adjprtma <djitothebeat123@gmail.com>
2021-10-21 20:16:41 +0700

    Sudah menyelesaikan video ke 12 tentang laravel pada pertemuan ke 7

--- coba-laravel/routes/web.php
+++ coba-laravel/routes/web.php
@@ -21,7 +21,8 @@ use App\Models\User;
 
 Route::get('/', function () {
     return view('home', [
-        "title" => "Home"
+        "title" => "Home",
+        "active" => 'home'
     ]);
 });
 
@@ -42,6 +43,7 @@ Route::get('posts/{post:slug}', [PostController::class, 'show']);
 Route::get('/categories', function() {
     return view('categories', [
         'title' => 'Post Categories',
+        'active' => 'categories',
         'categories' => Category::all()
     ]);
 });
@@ -49,6 +51,7 @@ Route::get('/categories', function() {
 Route::get('/categories/{category:slug}', function(Category $category) {
     return view('posts', [
         'title' => "Post by Category : $category->name",
+        'active' => 'categories',
         'posts' => $category->posts->load('category', 'author')
     ]);
 });
