5a4acd3a062223786af44b39199d7538fe63bdd5
adjprtma <djitothebeat123@gmail.com>
2021-11-24 14:23:49 +0700

    SUDAH menyelesaikan tugas video laravel ke 22 pada pertemuan ke 10

--- coba-laravel/resources/views/dashboard/posts/create.blade.php
+++ coba-laravel/resources/views/dashboard/posts/create.blade.php
@@ -40,7 +40,8 @@
             </div>
             <div class="mb-3">
               <label for="image" class="form-label">Post Image</label>
-              <input class="form-control @error('image') is-invalid @enderror" type="file" id="image" name="image">
+              <img class="img-preview img-fluid mb-3 col-sm-5 d-block" id="img-preview">
+              <input class="form-control @error('image') is-invalid @enderror" type="file" id="image" name="image" onchange="document.getElementById('img-preview').src = window.URL.createObjectURL(this.files[0])">
               @error('image')
                 <div class="invalid-feedback">
                   {{ $message }}
@@ -72,5 +73,20 @@
       document.addEventListener('trix-file-accept', function(e) {
         e.preventDefault();
       })
+
+      // function previewImage() {
+      //   const image = document.querySelector('#image');
+      //   const imgPreview = document.querySelector('.img-preview');
+
+      //   imgPreview.style.display = 'block';
+
+      //   const oFReader = new FileReader();
+      //   oFReader.readAsDataURL(image.files[0]);
+
+      //   oFReader.onload = function(oFReader) {
+      //     imgPreview.src = oFREvent.target.result;
+      //   }
+      // }
+
     </script>
 @endsection
